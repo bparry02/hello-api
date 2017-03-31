01564fe844d731f6891237823b44ff166865012a
Bryan Parry <bparry@redhat.com>
2017-03-31 16:01:33 -0400

    add hello endpoint

--- src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
+++ src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
@@ -17,9 +17,11 @@
 package org.jboss.as.quickstarts.rshelloworld;
 
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 /**
  * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
@@ -42,10 +44,10 @@ public class HelloWorld {
     }
 
     @GET
-    @Path("/xml")
-    @Produces({ "application/xml" })
-    public String getHelloWorldXML() {
-        return "<xml><result>" + helloService.createHelloMessage("World") + "</result></xml>";
+    @Path("/hello")
+    @Produces({ "application/json" })
+    public String sayHello(@QueryParam("name") @DefaultValue("World") String name) {
+    	return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
     }
 
 }
